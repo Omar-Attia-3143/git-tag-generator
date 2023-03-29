9e5f07d66476b6854eba5d945b8fbde34903a001
Omar Attia <omar.attia@syngenta.com>
2023-03-29 23:27:43 +0200

    add prod tags

--- GitTagGen.py
+++ GitTagGen.py
@@ -24,14 +24,21 @@ class GitTagGen:
         self.repo.create_tag(tag)
         self.repo.remote('origin').push(tag)
 
+    def generate_prod_tag(self):
+        tag = f'prod/{datetime.datetime.now().strftime("%Y-%m-%dT%H%M")}'
+        self.repo.create_tag(tag)
+        self.repo.remote('origin').push(tag)
+
 
 def main():
     try:
         option = int(input('please select an option \n'
                            ' 1.dev \n'
                            ' 2.qa \n'
-                           ' 3.uat \n'
-                           ' 4.dev & qa \n'))
+                           ' 3.dev & qa \n'
+                           ' 9.uat \n'
+                           ' 0.prod \n'
+                           ))
 
         tag_manager = GitTagGen()
 
@@ -40,10 +47,12 @@ def main():
         elif option == 2:
             tag_manager.generate_qa_tag()
         elif option == 3:
-            tag_manager.generate_uat_tag()
-        elif option == 4:
             tag_manager.generate_dev_tag()
             tag_manager.generate_qa_tag()
+        elif option == 9:
+            tag_manager.generate_uat_tag()
+        elif option == 0:
+            tag_manager.generate_prod_tag()
 
         print('pipline: https://app.circleci.com/pipelines/github/syngenta-digital \n')
     except Exception as error:
